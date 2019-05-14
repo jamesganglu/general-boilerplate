ce914222bfd47ac225985ccf5dd807bcb7777fe9
jamesganglu <jlu@ew-dc.com>
2019-05-14 15:54:52 -0400

    init

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+var arr1 = [1,2,3];
+
+var arr2  = arr1.map(a=>a*2);
+var p1 = new Promise();
+
+function a(){
+	console.log('a');
+}
+
+function b(){
+	console.log('b');
+}
+function c(){
+	console.log('c');
+}
---
+++ temp/js/bundle.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log('lib1');
+"use strict";
+
+console.log('lib2');
+"use strict";
+
+var arr1 = [1, 2, 3];
+var arr2 = arr1.map(function (a) {
+  return a * 2;
+});
+var p1 = new Promise();
+
+function a() {
+  var b = 100;
+}
